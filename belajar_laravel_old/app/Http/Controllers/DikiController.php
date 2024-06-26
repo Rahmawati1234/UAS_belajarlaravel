322393a9a40737099ad4519b66af97e217641458
Rahmawati1234 <rw9382154@gmail.com>
2024-06-26 01:47:06 -0700

    first commit

---
+++ belajar_laravel_old/app/Http/Controllers/DikiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Anggota;
+use App\Models\Hadiah;
+
+class DikiController extends Controller
+{
+    public function index()
+    {
+        $anggota = Anggota::all();
+        return view('anggota', ['anggota' => $anggota]);
+    }
+    public function hash(){
+        $hash_password_saya = Hash::make('halo123');
+        echo $hash_password_saya;
+        }
+}
---
+++ belajar_laravel_old/resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tutorial Laravel – www.eltacanss.com</title>
+</head>
+<body>
+ 
+<h3>Eltaa</h3>
+<p>Ig : @_x.lta</p>
+<p>Ini adalah blog Elta</p>
+ 
+</body>
+</html>
